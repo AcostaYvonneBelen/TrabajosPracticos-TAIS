1d9f093d3a8f4d303cc65b60c0d354930c942cce
yvonne <yvonne_belen_acosta@hotmail.com>
2019-08-20 15:19:21 -0300

    mi primer commit

---
+++ librarian/dbcon.php
@@ -0,0 +1,13 @@
+
+<?php
+$enlace = mysqli_connect("localhost", "root", "", "eb_lms");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+mysqli_close($enlace);
+?>
